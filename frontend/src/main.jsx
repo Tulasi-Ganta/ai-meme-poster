13ca8bc7cf53b640c1cfcf1f9c0474d12f04f849
Tulasi Ganta <gantatulasi2903@gmail.com>
2025-10-08 19:39:42 +0530

    Updated backend, frontend, README, screenshots; removed unnecessary files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import './style.css'
 import App from './App'
 import React from 'react'
